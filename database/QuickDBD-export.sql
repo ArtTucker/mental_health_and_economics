e2b970ce15429ff48ffb990531717f9a5532d300
Sylvain-SF <sylvain.dessagnes@gmail.com>
2021-04-26 16:58:03 -0700

    Upload database/

---
+++ database/QuickDBD-export.sql
@@ -0,0 +1,35 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Final_project_mental_health Database:
+
+CREATE TABLE "dataset_2014" (
+    "age" int   NOT NULL,
+    "gender" text   NOT NULL,
+    "country" text   NOT NULL,
+    "state" text   NOT NULL,
+    "self_employed" text   NOT NULL,
+    "family_history" text   NOT NULL,
+    "treatment" text   NOT NULL,
+    "work_interfere" text   NOT NULL,
+    "no_employees" text   NOT NULL,
+    "remote_work" text   NOT NULL,
+    "tech_company" text   NOT NULL,
+    "benefits" text   NOT NULL,
+    "care_options" text   NOT NULL,
+    "wellness_program" text   NOT NULL,
+    "seek_help" text   NOT NULL,
+    "anonymity" text   NOT NULL,
+    "leave" text   NOT NULL,
+    "mental_health_consequence" text   NOT NULL,
+    "phys_health_consequence" text   NOT NULL,
+    "coworkers" text   NOT NULL,
+    "supervisor" text   NOT NULL,
+    "mental_health_interview" text   NOT NULL,
+    "phys_health_interview" text   NOT NULL,
+    "mental_vs_physical" text   NOT NULL,
+    "obs_consequence" text   NOT NULL,
+    "comments" text   NOT NULL,
+    "year" int   NOT NULL
+);
+
